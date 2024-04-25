1bc6a9e95881b439110316a7e340237cf2c58535
Dustyn <dustyn_gibb@hotmail.com>
2024-04-25 16:20:22 -0400

    this should be a little better

--- src/enums/enums.js
+++ src/enums/enums.js
@@ -7,62 +7,53 @@ export const URI = {
 
     BASE_URL: "http://127.0.0.1:6969",
 
-    LOGIN: "/login",
-    SIGNUP: "/signup",
+    LOGIN: this.BASE_URL + this.BASE_URI + "/login",
+    SIGNUP: this.BASE_URL + this.BASE_URI + "/signup",
 
-    CHANGE_USERNAME: "/profile/changeUserName",
-    FETCH_PROFILE_BY_ID: "/profiles/get/",
-    CHANGE_PASSWORD: "/profile/changePassword",
-    DELETE_ACCOUNT: "/profile/deleteAccount",
-    CHANGE_BIO: "/profile/changeBio",
-    CHANGE_PUBLIC_KEY: "/profile/changePublicKey",
-    CHANGE_PROFILE_PHOTO: "/profile/changeProfilePhoto",
-    CHANGE_AUTO_ENCRYPTION_FLAG: "/profile/changeAutoEncryptionFlag",
+    CHANGE_USERNAME: this.BASE_URL + this.BASE_URI + "/profile/changeUserName",
+    FETCH_PROFILE_BY_ID: this.BASE_URL + this.BASE_URI + "/profiles/get/",
+    CHANGE_PASSWORD: this.BASE_URL + this.BASE_URI + "/profile/changePassword",
+    DELETE_ACCOUNT: this.BASE_URL + this.BASE_URI + "/profile/deleteAccount",
+    CHANGE_BIO: this.BASE_URL + this.BASE_URI + "/profile/changeBio",
+    CHANGE_PUBLIC_KEY: this.BASE_URL + this.BASE_URI + "/profile/changePublicKey",
+    CHANGE_PROFILE_PHOTO: this.BASE_URL + this.BASE_URI + "/profile/changeProfilePhoto",
+    CHANGE_AUTO_ENCRYPTION_FLAG: this.BASE_URL + this.BASE_URI + "/profile/changeAutoEncryptionFlag",
 
-    NOTIFICATION_FETCH : "/notifications/fetch",
-    NOTIFICATION_MARK_READ : "/notifications/markread/",
-    NOTIFICATION_MARK_UNREAD : "/notifications/markunread/",
+    NOTIFICATION_FETCH: this.BASE_URL + this.BASE_URI + "/notifications/fetch",
+    NOTIFICATION_MARK_READ: this.BASE_URL + this.BASE_URI + "/notifications/markread/",
+    NOTIFICATION_MARK_UNREAD: this.BASE_URL + this.BASE_URI + "/notifications/markunread/",
 
-    DELETE_COMMENT : "/comments/delete/",
-    GET_COMMENTS_FOR_POST : "/comments/get/",
-    GET_COMMENTS_BY_USER : "/comments/get/user/",
-    GET_CHILD_COMMENTS : "/comments/getchildren/",
-    LIKE_COMMENT : "/comments/like/",
-    DISLIKE_COMMENT : "/comments/dislike/",
-    UPDATE_COMMENT : "/comments/update/",
-    POST_COMMENT : "/comments/post",
+    DELETE_COMMENT: this.BASE_URL + this.BASE_URI + "/comments/delete/",
+    GET_COMMENTS_FOR_POST: this.BASE_URL + this.BASE_URI + "/comments/get/",
+    GET_COMMENTS_BY_USER: this.BASE_URL + this.BASE_URI + "/comments/get/user/",
+    GET_CHILD_COMMENTS: this.BASE_URL + this.BASE_URI + "/comments/getchildren/",
+    LIKE_COMMENT: this.BASE_URL + this.BASE_URI + "/comments/like/",
+    DISLIKE_COMMENT: this.BASE_URL + this.BASE_URI + "/comments/dislike/",
+    UPDATE_COMMENT: this.BASE_URL + this.BASE_URI + "/comments/update/",
+    POST_COMMENT: this.BASE_URL + this.BASE_URI + "/comments/post",
 
-    DELETE_POST : "/posts/delete/",
-    CREATE_POST : "/posts/create",
-    EDIT_POST : "/posts/edit/",
-    LIKE_POST : "/posts/like/",
-    DISLIKE_POST : "/posts/dislike/",
-    GET_ALL_POSTS : "/posts/posts/",
-    GET_POSTS_BY_TOPIC : "/posts/topic/",
-    SEARCH_POSTS : "/posts/search",
-    GET_POSTS_LIKE_OR_DISLIKED_BY_ME : "/posts/interacted/",
+    DELETE_POST: this.BASE_URL + this.BASE_URI + "/posts/delete/",
+    CREATE_POST: this.BASE_URL + this.BASE_URI + "/posts/create",
+    EDIT_POST: this.BASE_URL + this.BASE_URI + "/posts/edit/",
+    LIKE_POST: this.BASE_URL + this.BASE_URI + "/posts/like/",
+    DISLIKE_POST: this.BASE_URL + this.BASE_URI + "/posts/dislike/",
+    GET_ALL_POSTS: this.BASE_URL + this.BASE_URI + "/posts/posts/",
+    GET_POSTS_BY_TOPIC: this.BASE_URL + this.BASE_URI + "/posts/topic/",
+    SEARCH_POSTS: this.BASE_URL + this.BASE_URI + "/posts/search",
+    GET_POSTS_LIKE_OR_DISLIKED_BY_ME: this.BASE_URL + this.BASE_URI + "/posts/interacted/",
 
-    SEND_PRIVATE_MESSAGE : "/messages/send",
-    FETCH_ALL_MESSAGES : "/messages/send",
-    FETCH_MESSAGES_BY_SENDER : "/messages/fetchByUser",
-    GET_ALL_PEOPLE_WHO_MESSAGED_ME : "/messages/getAllSenders",
+    SEND_PRIVATE_MESSAGE: this.BASE_URL + this.BASE_URI + "/messages/send",
+    FETCH_ALL_MESSAGES: this.BASE_URL + this.BASE_URI + "/messages/send",
+    FETCH_MESSAGES_BY_SENDER: this.BASE_URL + this.BASE_URI + "/messages/fetchByUser",
+    GET_ALL_PEOPLE_WHO_MESSAGED_ME: this.BASE_URL + this.BASE_URI + "/messages/getAllSenders",
 
-    SUSPEND_USER : "/admin/suspend/",
-    UNSUSPEND_USER : "/admin/unsuspend/",
-    GIVE_ADMIN : "/admin/giveadmin/",
-    TAKE_ADMIN : "/admin/takeadmin/",
-    GET_ADMIN_LOGS : "/admin/getadminlogs/",
-    GET_ADMIN_LOGS_BY_ADMIN_ID : "/admin/getadminlogsforadmin/",
-    GET_SUSPEND_LOGS : "/admin/getsuspendlogs/"
-
-
-
-
-
-
-
-
-
+    SUSPEND_USER: this.BASE_URL + this.BASE_URI + "/admin/suspend/",
+    UNSUSPEND_USER: this.BASE_URL + this.BASE_URI + "/admin/unsuspend/",
+    GIVE_ADMIN: this.BASE_URL + this.BASE_URI + "/admin/giveadmin/",
+    TAKE_ADMIN: this.BASE_URL + this.BASE_URI + "/admin/takeadmin/",
+    GET_ADMIN_LOGS: this.BASE_URL + this.BASE_URI + "/admin/getadminlogs/",
+    GET_ADMIN_LOGS_BY_ADMIN_ID: this.BASE_URL + this.BASE_URI + "/admin/getadminlogsforadmin/",
+    GET_SUSPEND_LOGS: this.BASE_URL + this.BASE_URI + "/admin/getsuspendlogs/"
 
 
 }
